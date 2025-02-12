1644d19cdf164217e4973689f9157423f6b438b6
Akash Madavi <akashmadavi2001@gmail.com>
2025-02-12 09:26:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+var display = document.getElementById('display');
+var button = document.querySelectorAll('button');
+var string = "";
+var arr = Array.from(button);
+arr.forEach(button => {
+    button.addEventListener('click', (e) => {
+        if (e.target.innerHTML == '=') {
+            string = eval(string);
+            display.value = string;
+        }
+        else if (e.target.innerHTML == 'AC') {
+            string = '';
+            display.value = string;
+        }
+        else if (e.target.innerHTML == 'Del') {
+            string = string.substring(0, string.length - 1);
+            display.value = string;
+        }
+        else if (e.target.innerHTML == '%') {
+            string = string / 100;
+            display.value = string;
+        }
+        else {
+            string += e.target.innerHTML;
+            display.value = string;
+        }
+    })
+})
